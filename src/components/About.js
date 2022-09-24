e8a8fbcb23cbaf313f5d0fc4add140f8deb64eec
Jose Bosquez <josea_bosquez@outlook.com>
2022-09-23 18:12:33 -0600

    small html details included

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,56 @@
 import React from 'react'
+import "./about.css"
 
 function About() {
     return (
-        <div>About Me</div>
+        <div style={{  display: 'flex', flexDirection: "column", alignItems: 'center' }}>
+            <p>About Me</p>
+
+            <div style={{ fontSize: '15px', width: "60%"}}>
+                Hello, my name is Jose and I am a 6 year Marine Veteran. I began my coding journey in January 2021 at Digital Film Academy, Code-Immersives while undertaking a Javascript Full Stack course. I like to be challenged and provide solutions to modern day problems.
+            </div>
+            <br />
+            <br />
+            <div id="iconBox">
+                <div className="icons">
+                    <img className="imgs" src="https://img.icons8.com/ios/70/000000/html-5--v1.png" alt=""/>
+                    <p className="uls" >HTML5</p>
+                </div>
+                <div className="icons">
+                    <img className="imgs" src="https://img.icons8.com/ios/50/000000/css3.png" alt=""/>
+                    <p className="uls">CSS</p>
+                </div>
+
+                <div className="icons">
+                    <img className="imgs" src="https://img.icons8.com/ios/50/000000/javascript-logo.png" alt=""/>
+                    <p className="uls">Javascript</p>
+                </div>
+                <div className="icons">
+                    <img className="imgs" src="https://img.icons8.com/windows/50/000000/node-js.png" alt=""/>
+                    <p className="uls">Node.js</p>
+                </div>
+                <div className="icons">
+                    <img className="imgs" src="https://img.icons8.com/ios/50/000000/github--v1.png" alt=""/>
+                    <p className="uls">Github</p>
+                </div>
+                <div className="icons">
+                    <img className="imgs" src="https://img.icons8.com/ios/50/000000/git.png" alt=""/>
+                    <p className="uls">Git</p>
+                </div>
+                <div className="icons">
+                    <img className="imgs" src="https://img.icons8.com/carbon-copy/100/000000/react.png" alt=""/>
+                    <p className="uls">React</p>
+                </div>
+                <div className="icons">
+                    <img className="imgs" src="https://img.icons8.com/ios/50/000000/database.png" alt=""/>
+                    <p className="uls">Database</p>
+                </div>
+                <div className="icons">
+                    <img className="imgs" src="https://img.icons8.com/windows/50/000000/amazon-web-services.png" alt=""/>
+                    <p className="uls">AWS</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,27 +11,29 @@ function Contact() {
         console.log(lastNameInputValue.current.value)
         console.log(emailInputValue.current.value)
         console.log(messageInputValue.current.value)
-        e.preventDefault();
     }
 
     return (
         <div>
 
-            <p>Contact Me</p>
+            <p style={{ textAlign: 'center', marginTop: '50px'}}>Contact Me</p>
 
             <form style={{ alignContent: 'center', border: '1px solid black', width: '80%', height: '200px', marginLeft: '10%' }}>
                 <div style={{ display: 'flex', justifyContent: 'space-evenly', marginTop: '20px', marginBottom: '20px' }}>
-                    <input placeholder='First name' type="text" ref={firstNameInputValue} style={{ height: '25px' }} />
-                    <input placeholder='Last name' type="text" ref={lastNameInputValue} />
+                    <input placeholder='First name' type="text" ref={firstNameInputValue} style={{ height: '25px', width: '25%' }} />
+                    <input placeholder='Last name' type="text" ref={lastNameInputValue} style={{ height: '25px', width: "25%" }} />
+                    <input placeholder="Email" type="email" ref={emailInputValue} style={{ height: '25px', width: '25%' }} />
                 </div>
-                <input placeholder="Email" type="email" ref={emailInputValue} />
-                <input placeholder="Message" type="text" ref={messageInputValue} />
-                <button onClick={handleButton}>
-                    Enter
-                </button>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <textarea placeholder="Message" type="text" ref={messageInputValue} style={{ height: '50px', width: '80%', msTextOverflow: 'clip' }} />
+                    <br />
+                    <button onClick={handleButton}>
+                        Send Message
+                    </button>
+                </div>
             </form>
 
-            <div style={{ textAlign: "center", display: 'flex', justifyContent: 'space-evenly' }}>
+            <div style={{ textAlign: "center", display: 'flex', justifyContent: 'space-evenly', marginTop: '50px' }}>
                 <a href="https://www.linkedin.com/in/jose-bosquez-3599a0119?trk=people-guest_people_search-card" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/material-outlined/35/000000/linkedin--v2.png" alt="" /></a>
 
                 <a href="https://github.com/Josebosquez" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/windows/35/000000/github.png" alt="" /></a>
